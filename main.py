62dc4271032f2d5822cdc5ca02029c36f0aaed68
Atigorn.S <ati2rok@gmail.com>
2025-12-23 14:56:20 +0700

    baseline version 1.0

--- main.py
+++ main.py
@@ -1,23 +1,54 @@
 class Car:
-    def __init__(self, brand, model):
-        self.brand = brand
+    def __init__(self, model):
         self.model = model
-    
+        self.__mileage = 0 
+
     def drive(self):
-        print (f'{self.brand} , {self.model} is driving')
+        print(f"{self.model} is driving!")
+
+    def read_mileage(self):
+        print(f"This car has {self.__mileage} miles on it.")
+
+    def update_mileage(self, new_miles):
+        if new_miles >= self.__mileage:
+            self.__mileage = new_miles
+        else:
+            print("Error: You can't roll back mileage!")
+
+
+    def make_sound(self):
+        print("Beep Beep!")
+
 
 class ElectricCar(Car):
-    def __init__(self, model, brand, battery_size):
-        # ใช้ super() เพื่อดึงคุณสมบัติจากคลาสแม่ (Car)
-        super().__init__(model, brand)
+    def __init__(self, model, battery_size):
+        super().__init__(model)
         self.battery_size = battery_size
 
     def charge(self):
         print(f"The {self.battery_size}kWh battery is charging...")
 
-my_car = Car('Toyota', 'Vios')
-my_car.drive()
-my_ev = ElectricCar('Tesla', 'Model 3', 75)
+ 
+    def make_sound(self):
+        print("Silent Hum...")
+
+
+class Truck(Car):
+    def make_sound(self):
+        print("HONK HONK!")
+
+
+print("--- Testing Inheritance & Encapsulation ---")
+my_ev = ElectricCar("Tesla Model 3", 75)
 my_ev.drive()
 my_ev.charge()
+my_ev.update_mileage(150)
+my_ev.read_mileage()
+my_ev.update_mileage(100) 
 
+print("\n--- Testing Polymorphism ---")
+garage = [Car("Toyota"), ElectricCar("BYD", 60), Truck("Volvo")]
+
+for vehicle in garage:
+    print(f"{vehicle.model}: ", end="")
+    vehicle.make_sound()
